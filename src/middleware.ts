4c6cc5b1423324e62a270a50e19d8c6b3c226f80
qushoneo <pavelgrinevitsch2018@gmail.com>
2025-04-27 22:58:47 +0300

    disable middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,24 +4,25 @@ import { jwtVerify } from 'jose';
 const JWT_SECRET = process.env.JWT_SECRET || 'your-secret-key';
 
 export async function middleware(req: NextRequest) {
-  const token = req.cookies.get('token')?.value;
+  NextResponse.next();
+  // const token = req.cookies.get('token')?.value;
 
-  console.log(req.cookies);
+  // console.log(req.cookies);
 
-  if (!token) {
-    return NextResponse.redirect(new URL('/login', req.url));
-  }
+  // if (!token) {
+  //   return NextResponse.redirect(new URL('/login', req.url));
+  // }
 
-  try {
-    console.log('jwt verify');
-    await jwtVerify(token, new TextEncoder().encode(JWT_SECRET));
+  // try {
+  //   console.log('jwt verify');
+  //   await jwtVerify(token, new TextEncoder().encode(JWT_SECRET));
 
-    console.log('jwt verified successfully');
-    return NextResponse.next();
-  } catch (error) {
-    console.log('error while verification');
-    return NextResponse.redirect(new URL('/login', req.url));
-  }
+  //   console.log('jwt verified successfully');
+  //   return NextResponse.next();
+  // } catch (error) {
+  //   console.log('error while verification');
+  //   return NextResponse.redirect(new URL('/login', req.url));
+  // }
 }
 
 export const config = {
