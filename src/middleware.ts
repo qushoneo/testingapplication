d427c4a1d51d40a6e170baae0d82c5724e3de554
qushoneo <pavelgrinevitsch2018@gmail.com>
2025-04-27 23:02:50 +0300

    fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,27 +4,26 @@ import { jwtVerify } from 'jose';
 const JWT_SECRET = process.env.JWT_SECRET || 'your-secret-key';
 
 export async function middleware(req: NextRequest) {
-  NextResponse.next();
-  // const token = req.cookies.get('token')?.value;
+  const token = req.cookies.get('token')?.value;
 
-  // console.log(req.cookies);
+  if (!token) {
+    console.log('Token not found, redirecting to login...');
+    return NextResponse.redirect(new URL('/login', req.url));
+  }
 
-  // if (!token) {
-  //   return NextResponse.redirect(new URL('/login', req.url));
-  // }
+  try {
+    console.log('Attempting to verify JWT...');
 
-  // try {
-  //   console.log('jwt verify');
-  //   await jwtVerify(token, new TextEncoder().encode(JWT_SECRET));
+    await jwtVerify(token, new TextEncoder().encode(JWT_SECRET));
 
-  //   console.log('jwt verified successfully');
-  //   return NextResponse.next();
-  // } catch (error) {
-  //   console.log('error while verification');
-  //   return NextResponse.redirect(new URL('/login', req.url));
-  // }
+    console.log('JWT verified successfully');
+    return NextResponse.next();
+  } catch (error) {
+    console.log('Error during JWT verification:', error);
+    return NextResponse.redirect(new URL('/login', req.url));
+  }
 }
 
 export const config = {
-  matcher: ['/api/projects/:path*', '/api/users/:path'],
+  matcher: ['/api/projects/:path*', '/api/users/:path*'],
 };
