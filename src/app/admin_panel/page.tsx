ae23b959735d73f7d6be7c7350b5c85485514d33
qushoneo <pavelgrinevitsch2018@gmail.com>
2025-06-04 20:52:09 +0300

    fix server component

--- src/app/admin_panel/page.tsx
+++ src/app/admin_panel/page.tsx
@@ -1,18 +1,34 @@
-import axios from 'axios';
 import Image from 'next/image';
 
+async function getBugs() {
+  const baseUrl = process.env.API_URL || 'http://localhost:3000';
+  const res = await fetch(`${baseUrl}/api/bug`, { cache: 'no-store' });
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch bugs');
+  }
+
+  return res.json();
+}
+
 export default async function AdminPanel() {
-  const { data: bugs } = await axios.get('http://localhost:3000/api/bug');
+  const bugs = await getBugs();
 
   return (
-    <div className='flex flex-col gap-4 w-full p-4  '>
+    <div className='flex flex-col gap-4 w-full p-4'>
       {bugs?.map((bug: any) => (
         <div
           key={bug.id}
           className='flex gap-4 w-full border-b border-gray-200 pb-4 border-dashed items-center'
         >
           <p>{bug.text}</p>
-          <Image src={bug.screenshot} alt={bug.text} className='w-1/2 h-1/2' />
+          <Image
+            src={bug.screenshot}
+            alt={bug.text}
+            width={500}
+            height={300}
+            className='w-1/2 h-1/2'
+          />
         </div>
       ))}
     </div>
